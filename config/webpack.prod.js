3da7602005a32433ff3784b23104905b2cb54779
Kobi Mantzur <kobimantzur@gmail.com>
2019-07-31 17:04:11 +0300

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -69,7 +69,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': 'production'
+        'NODE_ENV': JSON.stringify(production)
       }
     })
   ]
